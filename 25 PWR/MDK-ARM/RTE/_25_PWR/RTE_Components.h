ad3ba6ea94d893edb796125f374e3de017d8d04d
guo-luchang <chang160528@163.com>
2024-02-24 10:46:06 +0800

    add 25 PWR project file

---
+++ 25 PWR/MDK-ARM/RTE/_25_PWR/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '25 PWR' 
+ * Target:  '25 PWR' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
